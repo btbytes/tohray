acc1ed41e2094bd3f6bbf30c3cf982da9ae23f4a
LV Nilesh <nilesh@cloudgeni.us>
2024-08-05 20:58:15 -0700

    Update example-consts.nim

--- example-consts.nim
+++ example-consts.nim
@@ -1,6 +1,6 @@
 
 # You MUST SET THESE VALUES before building
-#const dbPath* = "tohray.db" # full path to sqlite
+#const dbPath* = "/app/data/tohray.db" # full path to sqlite
 #const inviteCode* =         # something to share with anyone that you are inviting to the site. don't make it too easy to guess.
 #const secretKey* =          # something that looks like a s3cr37-an8-4-l0ng-0n3-much-l00nger-.
 #const siteName* =           # "My Blog"
