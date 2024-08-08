0bbc441bd1adb80e1e4a3d0fc06fec8e6b21f632
Pradeep Gowda <pradeep@btbytes.com>
2024-08-07 21:43:39 -0400

    Add Edit post and Delete post features (#2)
    * edit posts
    
    * implement edit and delete

--- urls.nim
+++ urls.nim
@@ -4,11 +4,12 @@ import ./views
 const urlPatterns* = @[
   pattern("/", homepage, HttpGet, name = "index"),
   pattern("/login", login, @[HttpGet, HttpPost], name = "login"),
-  pattern("/logout", logout, HttpGet),
-  pattern("/register", register, @[HttpGet, HttpPost]),
+  pattern("/logout", logout, HttpGet, name="logout"),
+  pattern("/register", register, @[HttpGet, HttpPost], name="register"),
   pattern("/write", createPost, @[HttpGet, HttpPost], name = "write"),
-  pattern("/delete", deletePost, @[HttpGet, HttpPost], name = "delete"),
+  pattern("/edit/{slug}", editPost, @[HttpGet, HttpPost], name = "edit"),
+  pattern("/delete/{slug}", deletePost, @[HttpGet, HttpPost], name = "delete"),
   pattern("/export", exportAll, HttpGet, name = "export"),
   pattern("/calendar", calendarView, HttpGet, name = "calendar"),
-  pattern("/{slug}", showPost),
+  pattern("/{slug}", showPost, name="post"),
 ]
